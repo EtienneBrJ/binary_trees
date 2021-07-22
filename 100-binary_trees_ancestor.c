606aedd78d99e1b0b3495c7cb8cdc52b714a84ac
EtienneBrJ <etiennebrxv@gmail.com>
2021-07-22 09:46:24 +0200

    Fixbetty

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -4,31 +4,31 @@
  * binary_trees_ancestor - finds the lowest common ancestor of two nodes
  * @first: first node
  * @second: second node
- * 
+ *
  * Return: pointer to the lowest common ancestor node of the two given nodes
  * or NULL
  */
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
-                                     const binary_tree_t *second)
+				     const binary_tree_t *second)
 {
-    int first_depth, second_depth;
+	int first_depth, second_depth;
 
-    if (!first || !second)
-        return (NULL);
+	if (!first || !second)
+		return (NULL);
 
-    first_depth = binary_tree_depth(first);
-    second_depth = binary_tree_depth(second);
+	first_depth = binary_tree_depth(first);
+	second_depth = binary_tree_depth(second);
 
-    for (; first_depth > second_depth; first = first->parent, first_depth--)
-        ;
-    for (; first_depth < second_depth; second = second->parent, second_depth--)
-        ;
-    for (; first && second; first = first->parent, second = second->parent)
-    {
-        if (first == second)
-            break;
-    }
-    return ((binary_tree_t *)first);
+	for (; first_depth > second_depth; first = first->parent, first_depth--)
+		;
+	for (; first_depth < second_depth; second = second->parent, second_depth--)
+		;
+	for (; first && second; first = first->parent, second = second->parent)
+	{
+		if (first == second)
+			break;
+	}
+	return ((binary_tree_t *)first);
 }
 
 /**
